55f17af251a68379410b73366e76107e364af51b
Calli Nutter <calli.c.nutter@gmail.com>
2020-04-24 15:26:15 +0200

    more static files additions...

--- cp_django_movie_app/settings.py
+++ cp_django_movie_app/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
