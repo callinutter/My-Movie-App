5d7e43bf1862b7e3a3144f5100f33e9862491457
Calli Nutter <calli.c.nutter@gmail.com>
2020-04-24 15:46:34 +0200

    changed application name

--- django_movie_app/urls.py
+++ django_movie_app/urls.py
@@ -1,4 +1,4 @@
-"""cp_django_movie_app URL Configuration
+"""django_movie_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
