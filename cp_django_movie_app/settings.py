3a002bd82676f561b13d8a821f20d50f5266d1c0
Calli Nutter <calli.c.nutter@gmail.com>
2020-04-24 16:34:45 +0200

    changed static root to movies folder

--- cp_django_movie_app/settings.py
+++ cp_django_movie_app/settings.py
@@ -120,13 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'movies', 'static')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
